9d453b9ac7a1b7f096bce60bf9c476c7bf90c939
Anna <luchinina.anna@gmail.com>
2018-04-11 09:21:20 +0300

    lab2

--- lab_1/findText/findText/test.bat
+++ lab_1/findText/findText/test.bat
@@ -3,28 +3,28 @@ set PROGRAM= "%~1"
 ECHO %~1
 
 REM запуск программы с включением строки в текстовом файле (английский язык)
-REM %PROGRAM% poem.txt "sky" > output.txt
-REM IF ERRORLEVEL 1 GOTO err
-REM FC output.txt poem_output.txt
-REM IF ERRORLEVEL 1 GOTO err
-
-REM запуск программы с включением строки в текстовом файле (кириллица)
-%PROGRAM% Onegin.txt "сомненья" > output.txt
+%PROGRAM% poem.txt "sky" > output1.txt
 IF ERRORLEVEL 1 GOTO err
-FC output.txt Onegin_output.txt
+FC output1.txt poem_output.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с пустым файлом
-REM %PROGRAM% empty.txt "sky" > output.txt
-REM IF ERRORLEVEL 1 GOTO err
-REM FC output.txt empty_output.txt
-REM IF ERRORLEVEL 1 GOTO err
+%PROGRAM% empty.txt "sky" > output3.txt
+IF ERRORLEVEL 1 GOTO err
+FC output3.txt empty_output.txt
+IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с текстовым файлом, не включающим искомое слово
-REM %PROGRAM% not_found.txt "there is no this text in file" > output.txt
-REM IF ERRORLEVEL 1 GOTO err
-REM FC output.txt not_found_output.txt
-REM IF ERRORLEVEL 1 GOTO err
+%PROGRAM% not_found.txt "there is no this text in file" > output4.txt
+IF ERRORLEVEL 1 GOTO err
+FC output4.txt not_found_output.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM запуск программы с включением строки в текстовом файле (кириллица)
+%PROGRAM% Onegin.txt "взгляд" > output2.txt
+IF ERRORLEVEL 1 GOTO err
+FC output2.txt Onegin_output.txt
+IF ERRORLEVEL 1 GOTO err
 
 ECHO Program testing succeeded
 EXIT 0
