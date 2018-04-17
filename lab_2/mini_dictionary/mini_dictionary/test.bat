1e3b23bbdcdac482af501e070d6d4dfc483d3748
Anna <luchinina.anna@gmail.com>
2018-04-18 01:02:27 +0300

    Update Mini-dictionary

--- lab_2/mini_dictionary/mini_dictionary/test.bat
+++ lab_2/mini_dictionary/mini_dictionary/test.bat
@@ -2,7 +2,7 @@ set PROGRAM= "%~1"
 ECHO %~1
 
 REM запуск программы с не единичной матрицей
-%PROGRAM% dictionary.txt > output.txt
+%PROGRAM% dictionary.txt> output.txt
 IF ERRORLEVEL 1 GOTO err
 output.txt dictionary_output.txt
 IF ERRORLEVEL 1 GOTO err
